cb07eb8092f9afc444d25d6e0d511532f5045373
AVNISH KUMAR <avnishkrsharma3@gmail.com>
2022-06-19 15:25:45 +0530

    Updates

---
+++ precache-manifest.983e9e6420fb1b2d3183c2b15f0b7141.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2170b2aa8f3441147020bbaefaa497e9",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "2697546426f6b295a289",
+    "url": "/airbnb-clone/static/css/2.51457ed1.chunk.css"
+  },
+  {
+    "revision": "4f6e58c1fd02e9f97430",
+    "url": "/airbnb-clone/static/css/main.801bd4c3.chunk.css"
+  },
+  {
+    "revision": "2697546426f6b295a289",
+    "url": "/airbnb-clone/static/js/2.e7e2450b.chunk.js"
+  },
+  {
+    "revision": "9efecc6e366a89e935d3722c67df4853",
+    "url": "/airbnb-clone/static/js/2.e7e2450b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6e58c1fd02e9f97430",
+    "url": "/airbnb-clone/static/js/main.babbfa20.chunk.js"
+  },
+  {
+    "revision": "f358a9c4d5787a3a04f2",
+    "url": "/airbnb-clone/static/js/runtime-main.fc586be8.js"
+  }
+]);
